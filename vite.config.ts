1f772c2ccf6ba0a3ebdd1892267440365e235d96
apmentorship <contact.amnnetwork@gmail.com>
2026-08-24 21:46:27 +0000

    change vit or smtr

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs.plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // tesyting
-  base: '/AP-Mentorship-Web/',
+  base: '/Ap-Mentorship-Web/', 
 })
